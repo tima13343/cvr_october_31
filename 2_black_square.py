7270c3358d9f5897645cf4c7a4dd1e5c18fdd899
tima13343 <82310001+tima13343@users.noreply.github.com>
2024-10-31 15:41:25 +0300

    Add files via upload

--- 2_black_square.py
+++ 2_black_square.py
@@ -1,7 +1,21 @@
 import cv2
 cap = cv2.VideoCapture(0)
-
+a = 100
 while True:
     ret, frame = cap.read()
+    print(frame.shape)
+    height, width, _ = frame.shape
+    frame[:a, :a] = [0, 0, 0]
+    frame[height - a:, :a] = [0, 0, 0]
+    frame[:a, width - a:] = [0, 0, 0]
+    frame[height - a:, width - a:] = [0, 0, 0]
+    frame[height // 2 - a//2: height//2 + a//2, width // 2 - a//2: width//2 + a//2] = [0, 0, 0]
+
     cv2.imshow('camera', frame)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1000)
+    print(key)
+    if key == ord(' '):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
