abf63bb61200403c118d8182a11dcf60f69fbe0e
tima13343 <82310001+tima13343@users.noreply.github.com>
2024-11-01 16:21:46 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -2,5 +2,5 @@ from 1 import YOLO
 
 # Предсказание на новом изображении
 model = YOLO('yolov8n.pt')
-results = model.predict(source="C:\Users\030\PycharmProjects\cvr\image\train", conf=0.5)
+results = model.predict(source="C:\Users\030\PycharmProjects\cvr\image\train", conf=0.5, save=True)
 results.show()  # Показываем изображение с предсказанными объектами
