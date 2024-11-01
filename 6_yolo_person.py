1f14778b176da7ca4c7bd7a39a8155175b1dd2fe
tima13343 <82310001+tima13343@users.noreply.github.com>
2024-11-01 12:14:40 +0300

    Add files via upload

---
+++ 6_yolo_person.py
@@ -0,0 +1,38 @@
+from ultralytics import YOLO
+import cv2
+from ultralytics.utils.plotting import Annotator  # ultralytics.yolo.utils.plotting is deprecated
+from pprint import pprint
+
+model = YOLO('yolov8n.pt')
+cap = cv2.VideoCapture(0)
+pprint(model.names)
+
+while True:
+    _, img = cap.read()
+
+    # BGR to RGB conversion is performed under the hood
+    # see: https://github.com/ultralytics/ultralytics/issues/2575
+    results = model.predict(img)
+
+    for r in results:
+
+        annotator = Annotator(img)
+
+        boxes = r.boxes
+        for box in boxes:
+            b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
+            c = box.cls
+            if model.names[int(c)] == 'person':
+                annotator.box_label(b, model.names[int(c)])
+                top, left, bottom, right = b.numpy().astype(int)
+                print(top, left, bottom, right)
+                img[left:right, top:bottom, 0] = 255
+
+    img = annotator.result()
+    cv2.imshow('YOLO V8 Detection', img)
+    print(img)
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
